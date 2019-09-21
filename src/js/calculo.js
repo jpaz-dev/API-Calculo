c62a89dbd47376c26703145210a5ba7a83fe12bc
John <JonathanNicolasPaz@Outlook.com>
2019-09-21 16:59:20 -0300

    Agrego archivos

---
+++ src/js/calculo.js
@@ -0,0 +1,16 @@
+const sumar = arr => arr.reduce((x, y) => { return x + y });
+const restar = arr => arr.reduce((x, y) => { return x - y });
+const multiplicar = arr => arr.reduce((x, y) => { return x * y });
+const dividir = arr => arr.reduce((x, y) => { return x / y });
+
+const textAnalice = (string) => {
+    
+};
+
+array = [1,2,3,4];
+
+console.log(array);
+console.log(sumar(array));
+console.log(restar(array));
+console.log(multiplicar(array));
+console.log(dividir(array));
