39edf036a48f3e4810e61986e557541de3903d63
John <JonathanNicolasPaz@Outlook.com>
2019-09-21 17:49:20 -0300

    21/9/19 5:49PM

--- server.js
+++ server.js
@@ -1,6 +1,60 @@
+const path = require('path');
 const express = require('express');
-const api = express();
+const calculadora = require(path.join(__dirname, 'src/js/calculo.js'));
 
-api.get('/sumar', (req, res) => {
-    
+const api = express();
+const puerto = 4040;
+
+const jsonResponce = (errString, dataInt) => {
+    const obj = {
+        err: errString,
+        data: dataInt
+    };
+    return JSON.stringify(obj);
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+api.post('/sumar', (req, res) => {
+    const input = req.body;
+    let suma = calculadora.sumar(input);
+    res.send(
+        jsonResponce(`No se pudo realizar la suma de ${input.toString}`),
+        suma
+    );
+});
+
+api.post('/restar', (req, res) => {
+    const input = req.body;
+    let resta = calculadora.restar(input);
+    res.send(
+        jsonResponce(`No se pudo realizar la resta de ${input.toString}`),
+        resta
+    );
+});
+
+api.post('/multiplicar', (req, res) => {
+    const input = req.body;
+    let multiplicacion = calculadora.multiplicar(input);
+    res.send(
+        jsonResponce(`No se pudo realizar la multiplicacion de ${input.toString}`),
+        multiplicacion
+    );
+});
+
+api.post('/dividir', (req, res) => {
+    const input = req.body;
+    let division = calculadora.dividir(input);
+    res.send(
+        jsonResponce(`No se pudo realizar la suma de ${input.toString}`),
+        division
+    );
+});
+
+app.listen(puerto, (err) => {
+    if(!err){
+        console.log(`Escuchando en el puerto ${puerto}`);
+    } else {
+        console.log(`Error: ${err}`);
+    }
 });
--- src/js/calculo.js
+++ src/js/calculo.js
@@ -3,14 +3,4 @@ const restar = arr => arr.reduce((x, y) => { return x - y });
 const multiplicar = arr => arr.reduce((x, y) => { return x * y });
 const dividir = arr => arr.reduce((x, y) => { return x / y });
 
-const textAnalice = (string) => {
-    
-};
-
-array = [1,2,3,4];
-
-console.log(array);
-console.log(sumar(array));
-console.log(restar(array));
-console.log(multiplicar(array));
-console.log(dividir(array));
+module.export = {sumar, restar, multiplicar, dividir};
