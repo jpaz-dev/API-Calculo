71b8ef50bf5a80aa6f5fbf102ea7fe7a96be822a
John <JonathanNicolasPaz@Outlook.com>
2019-09-22 01:32:04 -0300

    Commit 22/9/2019 1:31AM

---
+++ client/js/calculadora.js
@@ -0,0 +1,41 @@
+const output = document.getElementById('resultado');
+
+const llamarAPICalculator = (direccion, valor1, valor2) => {
+    const request = new XMLHttpRequest();
+
+    request.onload = () => {
+        let res = JSON.parse(request.responseText);
+        output.value = !res.err ? res.data : res.err;
+    };
+
+    request.open('GET', `${direccion}?num1=${valor1}&num2=${valor2}`);
+    request.send();
+};
+
+const sumar = () => {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+
+    llamarAPICalculator('/sumar', num1, num2);
+};
+
+const restar = () => {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+
+    llamarAPICalculator('/restar', num1, num2);
+};
+
+const dividir = () => {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+
+    llamarAPICalculator('/dividir', num1, num2);
+};
+
+const multiplicar = () => {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+
+    llamarAPICalculator('/multiplicar', num1, num2);
+};
--- src/js/calculo.js
+++
@@ -1,6 +0,0 @@
-const sumar = arr => arr.reduce((x, y) => { return x + y });
-const restar = arr => arr.reduce((x, y) => { return x - y });
-const multiplicar = arr => arr.reduce((x, y) => { return x * y });
-const dividir = arr => arr.reduce((x, y) => { return x / y });
-
-module.export = {sumar, restar, multiplicar, dividir};
