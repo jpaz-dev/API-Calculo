635b452e757eeb69b22cb612be1c1a19c1d660a9
John <JonathanNicolasPaz@Outlook.com>
2019-09-22 01:34:52 -0300

    Commit 22/9/2019 1:34AM

--- server.js
+++ server.js
@@ -1,59 +1,91 @@
+const ip = require('ip');
 const path = require('path');
 const express = require('express');
-const calculadora = require(path.join(__dirname, 'src/js/calculo.js'));
+const bodyParser = require('body-parser');
+const calculadora = require(path.join(__dirname , '/src/calculatorAPI.js'));
 
-const api = express();
+const app = express();
 const puerto = 4040;
+
+const inputValido = (input) => {
+    if(!input.num1 || !input.num2){
+        return false;
+    } else {
+        return true;
+    }
+}
 
 const jsonResponce = (errString, dataInt) => {
     const obj = {
         err: errString,
         data: dataInt
-    };
+    }
+    console.log(obj)
     return JSON.stringify(obj);
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/client/')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/client/views/'));
 
-api.post('/sumar', (req, res) => {
-    const input = req.body;
-    let suma = calculadora.sumar(input);
-    res.send(
-        jsonResponce(`No se pudo realizar la suma de ${input.toString}`),
-        suma
-    );
+app.get('/sumar', (req, res) => {
+    let input = req.query;
+    let output;
+    if(inputValido(input)){
+        output = jsonResponce( null, calculadora.sumar(input.num1, input.num2));
+    } else {
+        output = jsonResponce('No se ha podido realizar la suma, intentelo de nuevo.', null);
+    }
+    res.send(output);
 });
 
-api.post('/restar', (req, res) => {
-    const input = req.body;
-    let resta = calculadora.restar(input);
-    res.send(
-        jsonResponce(`No se pudo realizar la resta de ${input.toString}`),
-        resta
-    );
+app.get('/restar', (req, res) => {
+    let input = req.query;
+    let output;
+    if(inputValido(input)){
+        output = jsonResponce( null, calculadora.restar(input.num1, input.num2));
+    } else {
+        output = jsonResponce('No se ha podido realizar la resta, intentelo de nuevo.', null);
+    }
+    res.send(output);
 });
 
-api.post('/multiplicar', (req, res) => {
-    const input = req.body;
-    let multiplicacion = calculadora.multiplicar(input);
-    res.send(
-        jsonResponce(`No se pudo realizar la multiplicacion de ${input.toString}`),
-        multiplicacion
-    );
+app.get('/multiplicar', (req, res) => {
+    let input = req.query;
+    let output;
+    if(inputValido(input)){
+        output = jsonResponce( null, calculadora.multiplicar(input.num1, input.num2));
+    } else {
+        output = jsonResponce('No se ha podido realizar la multiplicacion, intentelo de nuevo.', null);
+    }
+    res.send(output);
 });
 
-api.post('/dividir', (req, res) => {
-    const input = req.body;
-    let division = calculadora.dividir(input);
-    res.send(
-        jsonResponce(`No se pudo realizar la suma de ${input.toString}`),
-        division
-    );
+app.get('/dividir', (req, res) => {
+    let input = req.query;
+    let output;
+    if(inputValido(input) && input.num2 != 0){
+        output = jsonResponce( null, calculadora.dividir(input.num1, input.num2));
+    } else {
+        output = jsonResponce('No se ha podido realizar la division, intentelo de nuevo.', null);
+    }
+    res.send(output);
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/help', (req, res) => {
+    let address = `${ip.address()}:${puerto}`;
+    res.render('help', {address});
 });
 
 app.listen(puerto, (err) => {
     if(!err){
         console.log(`Escuchando en el puerto ${puerto}`);
+        console.log(`Puede entrar desde: ${ip.address()}:${puerto}`);
     } else {
         console.log(`Error: ${err}`);
     }
